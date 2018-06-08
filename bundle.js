0eccfef0bbf0b4ca259bb23a35032014dbd56a12
Kirple <kurt@example.com>
2018-06-07 22:14:09 -0400

    Mod palindrome detector

--- bundle.js
+++ bundle.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-let Phrase = require("mhartl-palindrome");
+let Phrase = require("kurple-palindrome");
 
 let string = prompt("Please enter a string for palindrome testing:");
 let phrase = new Phrase(string);
@@ -10,7 +10,7 @@ if (phrase.palindrome()) {
   alert(`"${phrase.content}" is not a palindrome.`);
 }
 
-},{"mhartl-palindrome":2}],2:[function(require,module,exports){
+},{"kurple-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
 
 // Adds `reverse` to all strings.
@@ -18,10 +18,11 @@ String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
+// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  // Return content processed for palindrome testing.
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
     return this.letters().toLowerCase();
   }
@@ -30,18 +31,14 @@ function Phrase(content) {
   // For example:
   //   new Phrase("Hello, world!").letters() === "Helloworld"
   this.letters = function letters() {
-    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+    const lettersRegEx = /[a-z]/i;
+    return Array.from(this.content).filter(c => c.match(lettersRegEx)).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    if (this.letters()) {
-      return this.processedContent() === this.processedContent().reverse();
-    } else {
-      return false;
-    }
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
 
 },{}]},{},[1]);
