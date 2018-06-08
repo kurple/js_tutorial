0ae5e4e3d5a5f4313f04659da7fc4f0248703f87
Kirple <kurt@example.com>
2018-06-07 22:01:54 -0400

    Mod palindrome detector

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let Phrase = require("kurple-palindrome");
+let Phrase = require("mhartl-palindrome");
 
 let string = prompt("Please enter a string for palindrome testing:");
 let phrase = new Phrase(string);
