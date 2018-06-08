02a26e093f701441b0657ad7d5bb4e8d6c9d6869
Kirple <kurt@example.com>
2018-06-07 20:04:53 -0400

    Modify palindrome detector

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ let phrase = new Phrase(string);
 if (phrase.palindrome()) {
   alert(`"${phrase.content}" is a palindrome!`);
 } else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  alert(`"${phrase.content}" is not a palindrome.`);
 }
