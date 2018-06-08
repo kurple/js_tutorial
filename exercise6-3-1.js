9202cc08d892bbb6a3b846093e80dff333347a80
Kirple <kurt@example.com>
2018-06-07 20:27:30 -0400

    Start palindrome detector

--- exercise6-3-1.js
+++
@@ -1,6 +0,0 @@
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-function functionalProduct(array) {
-  return array.reduce((total, n) => total *= n);
-}
-console.log(functionalProduct(a));
--- exercise7-1-1.js
+++
@@ -1,9 +0,0 @@
-// Defines a Phrase object.
-function Phrase(content) {
-  this.content = content;
-
-  // Makes the phrase LOUDER.
-  this.louder = function() {
-    return this.content.toUpperCase();
-  }
-}
