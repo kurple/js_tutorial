b551f42adbb61547c8a40947a3af6bb2ca74e8fe
Kirple <kurt@example.com>
2018-06-12 20:01:12 -0400

    Update

--- bundle.js
+++ bundle.js
@@ -50,7 +50,11 @@ function Phrase(content) {
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.letters()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
 }
 
